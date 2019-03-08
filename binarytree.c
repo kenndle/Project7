eaa0f5516c52dc18412d52950c00ec07152cc1a6
kenndle <44220058+kenndle@users.noreply.github.com>
2019-03-07 21:04:02 -0700

    Add files via upload

---
+++ binarytree.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "binarytree.h"
+
+void initialize(binary_tree* bt) {
+  s->root = NULL;
+  s->treeElements = 0;
+}
+bool search(binary_tree* bt, int key){
+
+}
+void insert(binary_tree* bt, int item){
+
+}
+void printinorder(binary_tree* bt){
+
+}
+void printpreorder(binary_tree* bt){
+
+}
+void printpostorder(binary_tree* bt){
+
+}
+int btsize(binary_tree* bt){
+}
+int treeheight(binary_tree* bt){
+
+}
