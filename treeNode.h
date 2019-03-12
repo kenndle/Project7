61a9274d238e952e68b0ec6fec550db152b8dac8
kenndle <44220058+kenndle@users.noreply.github.com>
2019-03-12 14:52:27 -0600

    Update treeNode.h

--- treeNode.h
+++ treeNode.h
@@ -8,11 +8,12 @@ typedef struct tree_node_struct {
 	int val;
 } TreeNode;
 
+//Used to tell the user how to implement the order
 enum tree_order{PreOrder, InOrder, PostOrder};
 
 TreeNode* newNode(int val);
 TreeNode* searchNode(TreeNode* node, int target);
-void printOrder(TreeNode* node, enum tree_order order);
+void printOrder(TreeNode* node, enum tree_order order); //prints all orders depending on which order is specified
 int heightNode(TreeNode* node);
 
 #endif
