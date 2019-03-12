80c928d2736830c36c6359530edcd1baff93af28
kenndle <44220058+kenndle@users.noreply.github.com>
2019-03-12 14:16:02 -0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,41 @@
+
+int main(int argc, char* argv[]) {
+   FILE* inFile = NULL;
+
+   printf("argc: %d\n", argc);
+   for (i = 0; i < argc; ++i) {
+     printf("argv[%d]: %s\n", i, argv[i]);
+   }
+
+   if (argc < 2) {
+      printf("Could not open file\n");
+      return -1; // -1 indicates error
+   }
+
+   inFile = fopen(argv[1], "r");
+
+   if (inFile == NULL) {
+      printf("Could not open file myfile.txt.\n");
+      return -1; // -1 indicates error
+   }
+
+   printf("Print in order\n");
+   printinorder(&bt);
+   printf("\nPrint pre order\n");
+   printpreorder(&bt);
+   printf("\nPrint post order\n");
+   printpostorder(&bt);
+   printf("\n");
+   found = search(&bt,33);
+   if (found){
+     printf("FOUND 33\n");
+   }
+  found = search(&bt,38);
+  if (found) {
+    printf("FOUND 38\n");
+  }
+    printf("Number of elements in tree: %d\n", btsize(&bt));
+    printf("Tree height: %d\n", treeheight(&bt));
+
+    return 0;
+}
